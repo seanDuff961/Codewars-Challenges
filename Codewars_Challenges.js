74871e32e90e24297c5b6dd1bf9f1f7906bd1adb
Sean Duff <duffs@rv-c02c24kfmd6r.attlocal.net>
2020-08-19 08:54:06 -0500

    more level 6 challenges

--- Codewars_Challenges.js
+++ Codewars_Challenges.js
@@ -1,3 +1,30 @@
+//LONGEST ALPHABETICAL SUBSTRING - LVL 6
+
+function longest(str) {
+  const alphabet = {
+    'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,
+    'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16,
+    'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25
+  };
+  let longest = str[0];
+  let word = '';
+  for (let i = 1; i < str.length; i++) {
+    if (i === 1) {
+      word = str[0];
+    }
+    if (alphabet[str[i]] > alphabet[str[i - 1]] || alphabet[str[i]] === alphabet[str[i - 1]]) {
+      word += str[i];
+    }
+    else {
+      word = str[i];
+    }
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  }
+  return longest;
+}
+
 //THE VOWEL CODE - LVL 6
 
 function encode(string) {
--- scratch.js
+++ scratch.js
@@ -1,107 +1,35 @@
-// function repeatingChar(str){
-//   const result = [];
-//   const strArr = str.toLowerCase().split("").sort().join("").match(/(.)\1+/g);
-//   if (strArr != null) {
-//     strArr.forEach((elem) => {
-//       result.push(elem[0]);
-//     });
-//   }
-//   return result;
-// }
-
-// function blocks(s) {
-//   if (s.length === 0) {
-//     return '';
-//   }
-//   if (!/(.).*\1/.test(s) === true) {
-//     let lower = s.match(/[a-z]/g).join('');
-//     let upper = s.match(/[A-Z]/g).join('');
-//     let numbers = s.match(/[\d]/g).sort().join('');
-//     return lower + upper + numbers;
-//   }
-//   if (!/(.).*\1/.test(s) === false) {
-//     let repeatChar = (repeatingChar(s)[0]);
-//     let repeatRegex = new RegExp(repeatingChar(s)[0]);
-//     let repeatCount = s.match(/[repeatRegex]/gi).length;
-//     let nonAChars = s.match(/[^a]/gi).join('');
-//     function getPosition(string, subString, index) {
-//       return s.split(repeatChar, index).join(repeatChar).length;
-//     }
-//     let index = getPosition(s, repeatChar, 2);
-//     // console.log('indexxxxx', index);
-//     return s.slice(0, index) + nonAChars.slice(1) + ('-' + repeatChar).repeat(repeatCount - 1);
-//   }
-// }
-// console.log(blocks('abacad'));
-
-/* function sumOfMinimums(arr) {
-  // your code here
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    let el = arr[i];
-    sum += el.sort((a, b) => a - b)[0];
-  }
-  return sum;
-} */
-
-
-//https://www.codewars.com/kata/530e15517bc88ac656000716/train/javascript
-function rot13(message){
-  //your code here
-  const alphabet1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
-  const alphabet2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];  
-  let indexes = [];
-  //get indexes
-  for (let i = 0; i < message.length; i++) {
-    if (message[i].match(/[a-z]/)) {
-      indexes.push(alphabet1.indexOf(message[i]));
+function longest(str) {
+  const alphabet = {
+    'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,
+    'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16,
+    'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25
+  };
+  let longest = str[0];
+  let word = '';
+  let longestWords = [];
+  for (let i = 1; i < str.length; i++) {
+    // console.log('charrrr', str[i]);
+    // console.log('alphabet[str[i]]', alphabet[str[i]]);
+    // console.log('alphabet[str[i - 1]]', alphabet[str[i - 1]]);
+    if (i === 1) {
+      word = str[0];
     }
-    if (message[i].match(/[A-Z]/)) {
-      indexes.push(alphabet2.indexOf(message[i]));
+    if (alphabet[str[i]] > alphabet[str[i - 1]] || alphabet[str[i]] === alphabet[str[i - 1]]) {
+      word += str[i];
     }
     else {
-      indexes.push(' ');
+      word = str[i];
     }
+    if (word.length > longest.length) {
+      longest = word;
+      longestWords.push(longest);
+    }
+    // console.log('worddddd', word);
+    // console.log('longestttt', longest);
   }
-  console.log('indexessss', indexes);
-  let newIndexes = [];
-  //get indexes after rotating
-  while (indexes.length) {
-    let element = indexes.shift();
-    console.log('elementttt', element);
-    if (element + 13) 
-    //let newIndex = (indexes.shift() + 13);
-    if (element > 26) {
-      element = element - 26;
-    }
-    newIndexes.push(element);
-  }
-  console.log('newIndexessss', newIndexes);
-  //create final string with new indexes
-  let finalString = '';
-  for (let j = 0; j < message.length; j++) {
-    if (message[j].match(/[a-z]/)) {
-      finalString += alphabet1[newIndexes[j]];
-    }
-    if (message[j].match(/[A-Z]/)) {
-      finalString += alphabet2[[newIndexes[j]]]
-    }
-    if (message[j].match(/[!@#$%^&*(),.?":{}|<>+=]/)) {
-      finalString += message[j];
-    }
-    if (message[j].match(/[\d]/)) {
-      finalString += message[j];
-    }
-    if (message[j] === ' ') finalString += ' ';
-  }
-  return finalString;
+  return longest;
 }
-console.log(rot13("10+2 is twelve."));
 
-//t == 19
-// + 13 
-//g == 6
+console.log(longest('zjmibuzcimfrpriciuspkfvybkusdjeymcbrkozvdquc'));
 
-
-
-
+//aaaabbbbctt
